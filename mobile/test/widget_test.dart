df5ef8b5b0a4b032007ffec07874161262ee8017
ABDULLAHkap <m.abdullah.aac@gmail.com>
2026-08-09 00:27:16 +0500

    Fix Flutter widget test for WifiMotionApp

---
+++ mobile/test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:wifi_motion_sensing/main.dart';
+
+void main() {
+  test('WifiMotionApp can be created', () {
+    const app = WifiMotionApp();
+    expect(app, isA<WifiMotionApp>());
+  });
+}
